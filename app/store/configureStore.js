aad400384506612c801b9ae52a521ebd46b9fc6f
Cole Erickson <pawnstar4@gmail.com>
2017-07-24 20:17:19 -0600

    Fixed production error

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -12,7 +12,7 @@ const getMiddleware = ()=>{
   const middleware = applyMiddleware(routerMiddleware, thunk);
 
   if (process.env.NODE_ENV === 'development') return compose( middleware, DevTools.instrument() );
-  return applyMiddleware(middleware, thunk);
+  return middleware;
 };
 
 export function configureStore(initialState) {
